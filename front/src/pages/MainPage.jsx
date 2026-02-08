d87de4b72ad8ce13bcd18bcdddfc3e9ba8f9bbe1
Evgeny Lobanov <evgeny1602@gmail.com>
2026-02-08 12:52:04 +0700

    fixes

--- front/src/pages/MainPage.jsx
+++ front/src/pages/MainPage.jsx
@@ -2,9 +2,9 @@ import { Menu } from '@/ui/Menu'
 
 export function MainPage() {
   return (
-    <div className="flex flex-col items-center gap-20 mt-5 relative h-screen w-full">
+    <div className="flex flex-col items-center gap-20 mt-5 relative h-dvh w-full">
       Главная
-      <Menu className={'absolute bottom-5'} />
+      <Menu className={'fixed bottom-5'} />
     </div>
   )
 }
--- front/src/ui/MenuContainer.jsx
+++ front/src/ui/MenuContainer.jsx
@@ -5,7 +5,7 @@ export const MenuContainer = ({ children, className }) => {
     <div
       className={cn(
         className,
-        'flex flex-row flex-nowrap overflow-x-auto scroll-smooth snap-x gap-2 py-1 px-3 bg-white rounded-full shadow-sm max-w-full'
+        'flex flex-row flex-nowrap overflow-x-auto scroll-smooth snap-x gap-2 py-1 px-3 bg-white rounded-full shadow-sm w-full'
       )}
       style={{
         msOverflowStyle: 'none',
