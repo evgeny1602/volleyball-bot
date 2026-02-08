368782abe07f47211300856d206a90b7f343c1ab
Evgeny Lobanov <evgeny1602@gmail.com>
2026-02-08 12:58:39 +0700

    main menu fix

--- front/src/ui/MenuContainer.jsx
+++ front/src/ui/MenuContainer.jsx
@@ -5,7 +5,7 @@ export const MenuContainer = ({ children, className }) => {
     <div
       className={cn(
         className,
-        'flex flex-row flex-nowrap overflow-x-auto scroll-smooth snap-x gap-2 py-1 px-3 bg-white rounded-full shadow-sm w-full'
+        'flex flex-row flex-nowrap overflow-x-auto scroll-smooth snap-x gap-2 py-1 px-3 bg-white rounded-full shadow-sm w-auto max-w-[80%]'
       )}
       style={{
         msOverflowStyle: 'none',
