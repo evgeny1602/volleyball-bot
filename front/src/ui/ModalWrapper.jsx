8223459541bad1b05da3ba3863587f7eb8730621
Evgeny Lobanov <evgeny1602@gmail.com>
2026-02-10 11:37:43 +0700

    fix

--- front/src/ui/ModalWrapper.jsx
+++ front/src/ui/ModalWrapper.jsx
@@ -1,6 +1,6 @@
 export const ModalWrapper = ({ children }) => {
   return (
-    <div className="w-full p-4 flex flex-col bg-white rounded-3xl shadow-sm max-h-[90vh]">
+    <div className="w-full p-4 flex flex-col bg-white rounded-3xl shadow-sm max-h-[95vh]">
       {children}
     </div>
   )
