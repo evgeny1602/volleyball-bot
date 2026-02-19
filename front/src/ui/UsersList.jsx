0870386730a0d9755f8fd9f9f20f1569bb237cd3
Evgeny Lobanov <evgeny1602@gmail.com>
2026-02-19 16:37:42 +0700

    users count

--- front/src/ui/UsersList.jsx
+++ front/src/ui/UsersList.jsx
@@ -7,17 +7,28 @@ export const UsersList = ({ statuses }) => {
 
   if (isLoading) return <Loader variant="small" />
 
+  const usersToDisplay = data?.data.filter((user) =>
+    statuses.includes(user.status)
+  )
+
   return (
-    <div className="divide-y divide-gray-300">
-      {data?.data
-        .filter((user) => statuses.includes(user.status))
-        .map((user) => (
+    <>
+      <div className="w-full flex flex-col items-center -mt-1 mb-4">
+        <span className="bg-white dark:bg-gray-800 px-2 text-center text-gray-600 dark:text-gray-400 text-xs -mb-2 z-1">
+          Всего [{usersToDisplay.length}]
+        </span>
+        <div className="w-full border-b border-dashed border-gray-300 dark:border-gray-500"></div>
+      </div>
+
+      <div className="divide-y divide-gray-300">
+        {usersToDisplay.map((user) => (
           <UserCard
             key={user.id}
             className="pt-4 pb-4 last:pb-0"
             user={user}
           />
         ))}
-    </div>
+      </div>
+    </>
   )
 }
