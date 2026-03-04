6c6f4b816474e8aac80faffcc5e75710a0eac5e7
Evgeny Lobanov <evgeny1602@gmail.com>
2026-03-04 21:54:40 +0700

    fix

--- front/src/ui/AdminBadge.jsx
+++ front/src/ui/AdminBadge.jsx
@@ -1,12 +1,11 @@
 import { cn } from '@/utils/cn'
 
-const ADMINS_EXCLUDE_TG_IDS = ['450980607']
+const ADMINS_EXCLUDE_TG_IDS = [450980607]
 
 export const AdminBadge = ({ player, className }) => {
-  const isAdminPlayer =
-    player.role == 'admin' && !ADMINS_EXCLUDE_TG_IDS.includes(player.tg_id)
+  console.log(player)
 
-  if (!isAdminPlayer) {
+  if (player.role != 'admin' || ADMINS_EXCLUDE_TG_IDS.includes(player.tg_id)) {
     return null
   }
 
